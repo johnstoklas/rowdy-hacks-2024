26bf4cc6895365855084fb77d7dd89bacd83731b
Theodore Chan <theodorechan@dhcp-10-184-25-79.utsarr.net>
2024-10-26 13:54:38 -0500

    commit

--- src/components/Health/index.js
+++ src/components/Health/index.js
@@ -1,34 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./index.scss"
 import Loader from "react-loaders"
+import { Link, NavLink } from 'react-router-dom';
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Health = () => {
 
     return (
         <>
         <div className='container about-page'>
-            <div className='text-zone'>
-                <p>
-                    I'm a sophomore at Trinity, starting to look for internship 
-                    opportunities in the technology sector. As a computer sciences 
-                    major, I see myself closing the gap between different mathematical 
-                    fields and their ability to optimise algorithms used by the financial 
-                    sector.
-                </p>
-                <p>
-                    As such, I've taken multiple higher level mathematics classes, 
-                    was personally selected to be a peer tutor for upper-division 
-                    mathematics (calculus/linear algebra), and clinched second place 
-                    for our region within the International Collegiate Programming 
-                    Competition: Division II (ICPC 2024: Houston). 
-                </p>
-                <p>
-                    Now, I see myself applying that experience at companies that seek
-                    math-oriented computer programmers.
-                </p>
-            </div>
+            <nav>
+                <NavLink 
+                    exact="true" 
+                    activeclassname="active"
+                    to="/about"
+                >
+                    <FontAwesomeIcon icon={faHeart} color="#4d4d4e" />
+                </NavLink>
+                <div className='text-zone'>
+                    <p>
+                        I'm a sophomore at Trinity, starting to look for internship 
+                        opportunities in the technology sector. As a computer sciences 
+                        major, I see myself closing the gap between different mathematical 
+                        fields and their ability to optimise algorithms used by the financial 
+                        sector.
+                    </p>
+                    <p>
+                        As such, I've taken multiple higher level mathematics classes, 
+                        was personally selected to be a peer tutor for upper-division 
+                        mathematics (calculus/linear algebra), and clinched second place 
+                        for our region within the International Collegiate Programming 
+                        Competition: Division II (ICPC 2024: Houston). 
+                    </p>
+                    <p>
+                        Now, I see myself applying that experience at companies that seek
+                        math-oriented computer programmers.
+                    </p>
+                </div>
+            </nav>
         </div>
-        <Loader type="ball-pulse" />
+        <Loader type="pacman" />
         </>
     )
 }
