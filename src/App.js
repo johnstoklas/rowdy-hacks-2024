2e8246fb92d4ac43bee68f24e0cca03976be5b16
Theodore Chan <theodorechan@dhcp-10-184-25-79.utsarr.net>
2024-10-26 12:38:46 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout'
+import About from './components/About';
+import Home from './components/Home';
+import Health from './components/Health';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} /> 
+          <Route path="about" element={<About />} />
+          <Route path="health" element={<Health />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
