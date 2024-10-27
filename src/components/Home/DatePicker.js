32df2cdc453c7b790d913d3207c7e104c1a3af18
John Stoklas <jstoklas@trinity.edu>
2024-10-27 07:28:39 -0500

    update:

--- src/components/Home/DatePicker.js
+++ src/components/Home/DatePicker.js
@@ -1,37 +1,7 @@
 import React, { useState } from 'react'
 
 const DatePicker = () => {
-    /*
-    const [selectedDate, setSelectedDate] = useState('');
 
-    const handleDateChange = (event) => {
-        const date = event.target.value;
-        setSelectedDate(date);
-        updateHTMLDate(date);
-    };
-
-    const updateHTMLDate = (date) => {
-        var dateArray = date.split('-');
-        console.log(dateArray);
-        if(dateArray.length < 3) {
-            document.getElementById('month').innerHTML = 'JAN'
-            document.getElementById('year').innerHTML = '1985';            
-            document.getElementById('day').innerHTML = '01';
-        }
-        else {
-            document.getElementById('year').innerHTML = dateArray[0];
-            document.getElementById('day').innerHTML = dateArray[2];
-            document.getElementById('month').innerHTML = monthConvert(dateArray[1]);
-        }
-    }
-
-    const monthConvert = (val) => {
-        return months[val - 1];
-    } 
-
-    const months = ['JAN','FEB', 'MAY', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUG', 'SEPT', 'OCT', 'NOV', 'DEC']
-*/
-    
   return (
     <>
     <div class="date-box-container">
